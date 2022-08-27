920dc0bf1078e640b258a09cb97468f86bf7d90f
rehankausar <44466120+rehankausar@users.noreply.github.com>
2022-08-27 20:36:34 +0500

    practicing functions

---
+++ AIC/Q1/Assignments/Class 1/functions.py
@@ -0,0 +1,10 @@
+num1 = '10'
+num2 = 20
+# print(num1+num2);
+
+for star in range(20):
+    if star == 10:
+        for symbol in range(5):
+            print('=', end=' ');
+    else:
+        print('*', end=' ');
